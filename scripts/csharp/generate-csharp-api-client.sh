d0d1c00dde91a68229476e0406010a8c1586a03b
Dragos Rogojan <drogojan@salesforce.com>
2019-06-11 17:19:35 +0300

    Client side validation + refactor

--- scripts/csharp/generate-csharp-api-client.sh
+++ scripts/csharp/generate-csharp-api-client.sh
@@ -3,7 +3,21 @@
 source ./setup-variables.sh
 
 CLI_JAR="${SFMC_SWAGGER_ROOT_FOLDER}/cli/swagger-codegen-cli-2.4.4.jar"
-CUSTOM_GENERATOR_JAR="${SFMC_SWAGGER_ROOT_FOLDER}/generators/csharp/target/csharp-swagger-codegen-1.0.0.jar"
-java -cp ${CUSTOM_GENERATOR_JAR}:${CLI_JAR} io.swagger.codegen.SwaggerCodegen generate -l csharp -i ${SFMC_SWAGGER_ROOT_FOLDER}/openapi-2.0/sfmc-openapi-v2.json -o ${CSHARP_SDK_GIT_REPO_FOLDER} -c ${SFMC_SWAGGER_ROOT_FOLDER}/openapi-2.0/swagger-codegen-config.json
+CSHARP_TEMPLATES_PATH="${SFMC_SWAGGER_ROOT_FOLDER}/templates/csharp"
+CURRENT_YEAR=`date +'%Y'`
+
+java -jar ${CLI_JAR} generate \
+    -l csharp \
+    -i ${SFMC_SWAGGER_ROOT_FOLDER}/openapi-2.0/sfmc-openapi-v2.json \
+    -t ${CSHARP_TEMPLATES_PATH} \
+    -o ${CSHARP_SDK_GIT_REPO_FOLDER} \
+    -c ${SFMC_SWAGGER_ROOT_FOLDER}/openapi-2.0/swagger-codegen-config.json \
+    -DmodelTests=false \
+    --additional-properties packageTitle="Salesforce Marketing Cloud C# SDK" \
+    --additional-properties packageCompany="Salesforce" \
+    --additional-properties packageProductName="Salesforce Marketing Cloud C# SDK" \
+    --additional-properties packageDescription="The Salesforce Marketing Cloud C# SDK" \
+    --additional-properties packageAuthors="Salesforce" \
+    --additional-properties packageCopyright="Copyright © Salesforce ${CURRENT_YEAR}" \
 
 exit $?
