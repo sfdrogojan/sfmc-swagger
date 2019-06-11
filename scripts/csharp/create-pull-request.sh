49438e8c6f2a8ef238dd699b00ed033024157dfd
Dragos Rogojan <drogojan@salesforce.com>
2019-06-11 18:30:58 +0300

    Fixed hub command

--- scripts/csharp/create-pull-request.sh
+++ scripts/csharp/create-pull-request.sh
@@ -20,7 +20,7 @@ pushd $CSHARP_SDK_GIT_REPO_FOLDER
 git fetch
 git checkout $BRANCH_NAME
 
-TRIGGERED_BY_PULL_REQUEST=`hub pr list -b dev -f "%U"`
+TRIGGERED_BY_PULL_REQUEST=`hub pr list -f "%U"`
 
 hub pull-request -m "Automation pipeline update" -m "PR created due to ${TRIGGERED_BY_PULL_REQUEST}"
 
