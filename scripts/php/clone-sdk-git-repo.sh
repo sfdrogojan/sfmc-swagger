340958aeed72101df997cc10f0fcdf0b0e162f6d
sfadincescu <adincescu@salesforce.com>
2019-08-26 13:13:44 +0300

    WIP

--- scripts/php/clone-sdk-git-repo.sh
+++ scripts/php/clone-sdk-git-repo.sh
@@ -4,3 +4,11 @@ source ../setup-variables.sh
 source ../common-functions.sh
 
 clone_sdk_git_repo ${PHP_SDK_GIT_REPO_ID}
+
+
+# Checkout relevant branch (testing only)
+pushd ${PHP_SDK_GIT_REPO_FOLDER}
+
+git checkout feature/client-validation
+
+popd
