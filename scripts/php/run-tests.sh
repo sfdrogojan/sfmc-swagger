3d0a8b27df5da1606b17c81ce85db27ab67da146
sfadincescu <adincescu@salesforce.com>
2019-08-26 12:42:39 +0300

    WIP

--- scripts/php/run-tests.sh
+++ scripts/php/run-tests.sh
@@ -2,7 +2,4 @@
 
 source ../setup-variables.sh
 
-bash ${PHP_SDK_GIT_REPO_FOLDER}/SalesForce/vendor/bin/phpunit -c ${PHP_SDK_GIT_REPO_FOLDER}/SalesForce/phpunit.xml.dist --testsuite=php-sdk-api,php-sdk-auth
-exit $?
-
-popd
+phpunit -c ${PHP_SDK_GIT_REPO_FOLDER}/SalesForce/phpunit.xml.dist --testsuite=php-sdk-api,php-sdk-auth
