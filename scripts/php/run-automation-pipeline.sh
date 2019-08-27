d50e8256d0e4bd60c0eb579a731e295f97755b75
sfadincescu <adincescu@salesforce.com>
2019-08-27 16:55:06 +0300

    WIP

--- scripts/php/run-automation-pipeline.sh
+++ scripts/php/run-automation-pipeline.sh
@@ -4,9 +4,9 @@ source ../setup-variables.sh
 source ../common-functions.sh
 
 # Preparing the environment
-deb http://archive.ubuntu.com/ubuntu/ bionic main
-deb http://archive.ubuntu.com/ubuntu/ bionic-updates main
-deb http://security.ubuntu.com/ubuntu bionic-security main
+echo "deb http://archive.ubuntu.com/ubuntu/ bionic main" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ bionic-updates main" >> /etc/apt/sources.list
+echo "deb http://security.ubuntu.com/ubuntu bionic-security main" >> /etc/apt/sources.list
 
 sudo apt-get update && sudo apt-get install -y php7
 pip install --user requests
